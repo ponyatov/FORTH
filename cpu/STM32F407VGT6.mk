368777c7e608280aae8efb465ab6c9ab1ebd2739
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-11 04:12:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpu/STM32F407VGT6.mk
+++ cpu/STM32F407VGT6.mk
@@ -3,3 +3,9 @@ ARCH = CortexM4
 OBJ += tmp/startup_stm32f407xx.o
 
 TCFLAGS += -DSTM32F407xx
+
+REF += ref/Components/lis302dl/README.md
+ref/Components/lis302dl/README.md:
+	$(GITREF) git@github.com:ponyatov/stm32-lis302dl.git $(dir $@)
+
+TCFLAGS += -Iref/Components
