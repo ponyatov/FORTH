21e33f744238b718ef83613ced52f73b6229110f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-23 12:07:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/language/forth-validator.ts
+++ src/language/forth-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { ForthAstType, Person } from './generated/ast.js';
+import type { ForthAstType, Forth } from './generated/ast.js';
 import type { ForthServices } from './forth-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: ForthServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.ForthValidator;
     const checks: ValidationChecks<ForthAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        Forth: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: ForthServices) {
  */
 export class ForthValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
+    checkPersonStartsWithCapital(forth: Forth, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
     }
 
 }
