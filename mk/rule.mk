0daf1be21c91edb672c3fbd2d216f8d0842a861b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-30 02:35:16 +0400

    .

--- mk/rule.mk
+++ mk/rule.mk
@@ -5,7 +5,7 @@ tmp/slide/%.mp3: tmp/slide/%.md mk/rule.mk
 MDS = $(wildcard doc/slide/*.md)
 MP3 = $(subst doc/,tmp/, $(subst .md,.mp3,$(MDS)))
 PNG = doc/splash.png
-VIDEO = -c:v libx264 -vf "hflip,scale=1280:720,fps=1" -r 1 -preset ultrafast -crf 28
+VIDEO = -c:v libx264 -vf "scale=1280:720,fps=1" -r 1 -preset ultrafast -crf 28
 AUDIO = -c:a aac -b:a 128k -ac 1 -ar 44100
 .PHONY: video
 video: tmp/slide/slide.mp4
