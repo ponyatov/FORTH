5d5c63fbb552c0e848329e22645286798a987d06
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-30 14:58:05 +0400

    .

--- net/pcap.py
+++ net/pcap.py
@@ -6,13 +6,15 @@ class Node: pass
 
 class Send(Node):
     name = 'veths'
-    mac = "00:11:22:33:44:55"
-    ip = "1.2.3.4"
+    mac = "e2:9b:e8:7a:a2:97"
+    ip = "111.111.111.111"
+    port = 5432
 
 class Recv(Node):
     name = 'vethr'
-    mac = "66:77:88:99:aa:bb"
-    ip = "5.6.7.8"
+    mac = "d6:4e:45:79:3c:e6"
+    ip = "111.111.111.222"
+    port = 12345
 
 # sender
 veths = pcpp.RawSocket(Send.name)
@@ -27,3 +29,9 @@ eth = pcpp.EthLayer(src_mac_addr=Send.mac,
                     dst_mac_addr=Recv.mac)
 ip = pcpp.IPv4Layer(src_addr=Send.ip,
                     dst_addr=Recv.ip)
+udp = pcpp.UdpLayer(src_port=Send.port,
+                    dst_port=Recv.port)
+
+packet / eth / ip / udp
+
+# sudo nc -u -l 111.111.111.222 -p 12345
