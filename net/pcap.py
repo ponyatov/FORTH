a1f477dbb383886a7f494ea1459ff7bdf2a9f786
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-30 13:34:00 +0400

    .

--- net/pcap.py
+++ net/pcap.py
@@ -2,7 +2,28 @@
 
 import py_pcapplusplus as pcpp
 
+class Node: pass
+
+class Send(Node):
+    name = 'veths'
+    mac = "00:11:22:33:44:55"
+    ip = "1.2.3.4"
+
+class Recv(Node):
+    name = 'vethr'
+    mac = "66:77:88:99:aa:bb"
+    ip = "5.6.7.8"
+
 # sender
-veths = pcpp.RawSocket('veths')
+veths = pcpp.RawSocket(Send.name)
 # receiver
-vethr = pcpp.RawSocket('vethr')
+vethr = pcpp.RawSocket(Recv.name)
+
+# new packet
+packet = pcpp.Packet()
+
+# add layers
+eth = pcpp.EthLayer(src_mac_addr=Send.mac,
+                    dst_mac_addr=Recv.mac)
+ip = pcpp.IPv4Layer(src_addr=Send.ip,
+                    dst_addr=Recv.ip)
