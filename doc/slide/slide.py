81698f64c6ce4354f853a7e12acfcce4d9018454
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-30 11:50:26 +0400

    .

--- doc/slide/slide.py
+++ doc/slide/slide.py
@@ -1,17 +1,29 @@
-import os, sys
+import os, sys, re
 
 try: os.mkdir('tmp/slide')
 except FileExistsError: pass
 
-for md in sorted(os.listdir('doc/slide')):
-    name, ext = md.split('.')
-    with open('tmp/slide/m3u.m3u', 'w') as m3u:
-        with open('tmp/slide/files.audio', 'w') as audio:
-            if ext == 'md':
-                with open(f'doc/slide/{name}.md', 'r') as r:
-                    text = r.read().split('# ru')[-1]
-                    with open(f'tmp/slide/{name}.md', 'w') as w:
-                        print(text, file=w)
-                os.system(f'make tmp/slide/{name}.mp3')
-                # print(f'{name}.mp3', file=m3u)
-                print(f'file {name}.mp3')#, file=audio)
+with open('tmp/slide/m3u.m3u', 'w') as m3u:
+    for md in sorted(os.listdir('doc/slide')):
+        name, ext = md.split('.')
+        if re.match(r'\d+',name):
+            match ext:
+                case 'md':
+                    with open(f'doc/slide/{name}.md', 'r') as r:
+                        text = r.read().split('# ru')[-1]
+                        with open(f'tmp/slide/{name}.ru.md', 'w') as w:
+                            print(text, file=w)
+                        # os.system(f'make tmp/slide/{name}.ru.mp3')
+                    print(f'file {name}.ru.mp3', file=m3u)
+                case 'png':
+                    os.system(f'cp doc/slide/{name}.png tmp/slide/{name}.png')
+                    print(f'file {name}.png', file=m3u)
+
+    #     with open('tmp/slide/files.audio', 'w') as audio:
+    #         if ext == 'md':
+    #             with open(f'doc/slide/{name}.md', 'r') as r:
+    #                 text = r.read().split('# ru')[-1]
+    #                 with open(f'tmp/slide/{name}.md', 'w') as w:
+    #                     print(text, file=w)
+    #             # print(f'{name}.mp3', file=m3u)
+    #             print(f'file {name}.mp3')#, file=audio)
--- mk/rule.mk
+++ mk/rule.mk
@@ -1,13 +1,13 @@
-VOICE = pavel
-tmp/slide/%.mp3: tmp/slide/%.md mk/rule.mk
-	RHVoice-test -i $< -o $@ -p $(VOICE)
+RU = pavel
+tmp/slide/%.ru.mp3: tmp/slide/%.ru.md mk/rule.mk
+	RHVoice-test -i $< -o $@ -p $(RU)
 
-MDS = $(wildcard doc/slide/*.md)
-MP3 = $(subst doc/,tmp/, $(subst .md,.mp3,$(MDS)))
-PNG = doc/splash.png
-VIDEO = -c:v libx264 -vf "scale=1280:720,fps=1" -r 1 -preset ultrafast -crf 28
-AUDIO = -c:a aac -b:a 128k -ac 1 -ar 44100
-.PHONY: video
-video: tmp/slide/slide.mp4
-tmp/slide/slide.mp4: tmp/slide/files.audio $(MP3) $(PNG) mk/rule.mk
-	ffmpeg -loop 1 -i $(PNG) -f concat -i $< $(VIDEO) $(AUDIO) -shortest $@
+# MDS = $(wildcard doc/slide/*.md)
+# MP3 = $(subst doc/,tmp/, $(subst .md,.mp3,$(MDS)))
+# PNG = doc/splash.png
+# VIDEO = -c:v libx264 -vf "scale=1280:720,fps=1" -r 1 -preset ultrafast -crf 28
+# AUDIO = -c:a aac -b:a 128k -ac 1 -ar 44100
+# .PHONY: video
+# video: tmp/slide/slide.mp4
+# tmp/slide/slide.mp4: tmp/slide/files.audio $(MP3) $(PNG) mk/rule.mk
+# 	ffmpeg -loop 1 -i $(PNG) -f concat -i $< $(VIDEO) $(AUDIO) -shortest $@
