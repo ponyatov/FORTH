0b017f17f0c7b31047b1d8d77c951b1f9eed90a4
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-03 11:59:03 +0400

    .

--- doc/slide/slide.py
+++ doc/slide/slide.py
@@ -76,8 +76,8 @@ def vid(k,x=1250,y=450,w=640,h=360):
 
 vid('14',700,70)
 
-vid('18')
-single('18')
+vid('19')
+single('19')
 
 with open('tmp/slide/fragments.list', 'w') as list:
     with open('tmp/slide/m3u.m3u', 'w') as m3u:
