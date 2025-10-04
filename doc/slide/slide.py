d3307f1dec3fa640c3a1edf00cdad045f2d53084
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-04 07:21:24 +0400

    .

--- doc/slide/slide.py
+++ doc/slide/slide.py
@@ -1,4 +1,4 @@
-import os, sys, re
+import os, re
 import ffmpeg
 
 try: os.mkdir('tmp/slide')
@@ -35,50 +35,37 @@ def slide(k, video, audio, dura):
     video = ffmpeg.input(video, loop=1, framerate=1)
     audio = ffmpeg.input(audio)
     stream = ffmpeg.output(
-        video,
-        audio,
-        f'tmp/slide/{k}.mp4',
-        t=dura,
+        video, audio, f'tmp/slide/{k}.mp4', t=dura,
         **{
             # video codec
-            'c:v': 'libx264',
-            'pix_fmt': 'yuv420p',
-            'vf': 'scale=1280:720,fps=1',
-            'r': 1,
-            'preset': 'ultrafast',
-            'crf': 28,
+            'c:v': 'libx264', 'pix_fmt': 'yuv420p', 'r': 1, 'crf': 28,
+            'vf': 'scale=1280:720,fps=1', 'preset': 'ultrafast',
             # audio codec
-            'c:a': 'aac',
-            'b:a': '128k',
-            'ac': 1,
-            'ar': 44100,
-            'tune': 'stillimage',
-            # Use 'shortest' to stop when the audio ends
-            # 'shortest': None,
+            'c:a': 'aac', 'b:a': '128k', 'ac': 1,
+            'ar': 44100, 'tune': 'stillimage',
         }
     )
     stream = ffmpeg.overwrite_output(stream)
     ffmpeg.run(stream)
 
-def single(k):
-    tts(k)
-    # os.system(f'cvlc tmp/slide/{k}.mp3')
-    png = f'doc/slide/{k}.png'# fragment[k]['png']
-    mp3 = f'tmp/slide/{k}.mp3'# fragment[k]['mp3']
-    probe = ffmpeg.probe(mp3)
-    dura = float(probe['streams'][0]['duration'])
-    print(k, png, mp3, dura)
-    slide(k, png, mp3, dura)
-    os.system(f'cvlc tmp/slide/{k}.mp4')
-
-def vid(k,x=1250,y=440,w=640,h=360):
+def grab(k,x=1250,y=440,w=640,h=360):
     os.system(f'import -window root -crop {w}x{h}+{x}+{y} png:doc/slide/{k}.png')
 
-vid('29',700,70)
+def single(k):
+    tts(k)
+    png = f'doc/slide/{k}.png'# fragment[k]['png']
+    mp3 = f'tmp/slide/{k}.mp3'# fragment[k]['mp3']
+    probe = ffmpeg.probe(mp3); dura = float(probe['streams'][0]['duration'])
+    print(k, png, mp3, dura); slide(k, png, mp3, dura)
+    os.system(f'cvlc --play-and-exit tmp/slide/{k}.mp4')
+
+# acer
+grab('29',710,70)
 single('29')
 
-vid('27',y=430)
-single('27')
+# mas
+grab('29',y=430)
+single('29')
 
 with open('tmp/slide/fragments.list', 'w') as list:
     with open('tmp/slide/m3u.m3u', 'w') as m3u:
