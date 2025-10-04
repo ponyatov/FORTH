cb73cb0ded8bc51bc98051e49677e06bd8fc3c38
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-04 08:17:30 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lib/FORTH_test.py
@@ -0,0 +1,14 @@
+# from unittest import TestCase
+
+# class TryTesting(TestCase):
+#     def test_always_passes(self):
+#         self.assertTrue(True)
+
+#     def test_always_fails(self):
+#         self.assertTrue(False)
+
+def test_always_passes():
+    assert True
+
+def test_always_fails():
+    assert False
