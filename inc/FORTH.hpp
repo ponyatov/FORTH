3d22e25cd359fc01cf7bd3461a01789f6de71f8d
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-05 23:15:29 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/FORTH.hpp
+++ inc/FORTH.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int main(int argc, char *argv[]);
+extern void arg(int argc, char *argv);
+
 extern int yylex();
 extern int yylineno;
+extern char* yytext;
+extern char* yyfile;
+extern FILE* yyin;
 extern int yyparse();
-extern void yyerror(const char *msg);
+extern void yyerror(const char* msg);
 #include "FORTH.yacc.hpp"
